c855652a4546f32c476cd30f135e93f283790e3c
Lukas Behammer <131662795+lukasbehammer@users.noreply.github.com>
2023-12-30 18:58:44 +0100

    add week-4 examples4

---
+++ examples4/addresses.c
@@ -0,0 +1,14 @@
+#include "../cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    // int n = 50;
+    // int *p = &n;
+    // printf("%i\n", *p);
+
+    char *s = "Hi!";
+    printf("%s\n", s);
+    printf("%s\n", s+1);
+    printf("%s\n", s+2);
+}
